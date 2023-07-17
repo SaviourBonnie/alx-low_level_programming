c089506e0436ef165166bcd32125a290f1eee6a8
SaviourBonnie <saviourbonnie@gmail.com>
2023-07-17 05:16:48 -0700

    my first C program

---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+This projects has a script that runs C file through the preprocessor and saves the result into another file
---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Entrpoint
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	return (0);
+}
