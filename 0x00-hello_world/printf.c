9971e7243f94aa45e7577fabce8f166165a2f274
SaviourBonnie <saviourbonnie@gmail.com>
2023-07-17 08:30:40 -0700

    using printf

---
+++ 0x00-hello_world/printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Aways 0 (Success)
+ */
+int main(void)
+{
+	printf("With proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
