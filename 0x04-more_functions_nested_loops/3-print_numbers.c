6f662d7ba8ad409e4d086cf3ec5bb1894b20ede7
SaviourBonnie <saviourbonnie@gmail.com>
2023-07-20 17:13:01 -0700

    func 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+    int num;
+
+    for (num = 0; num < 10; num++)
+        _putchar(num + '0');
+
+    _putchar('\n');
+}
+
