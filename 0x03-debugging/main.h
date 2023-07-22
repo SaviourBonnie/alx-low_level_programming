07ee2c38c80038dfbebf990f66a3c34ba4d30d91
SaviourBonnie <saviourbonnie@gmail.com>
2023-07-21 19:51:08 -0700

    debg 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,4 +4,16 @@
 /* Function prototype for positive_or_negative() */
 void positive_or_negative(int i);
 
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c);
+
+// Add any other existing function prototypes or declarations below this line if they exist
+
 #endif /* MAIN_H */
+
