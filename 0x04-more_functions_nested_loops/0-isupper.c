e6ee8cda5e118db3bfe99507b496100332ed30a2
SaviourBonnie <saviourbonnie@gmail.com>
2023-07-21 20:42:46 -0700

    putchar

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,13 @@
+#include "main.h"
+
 /**
- * _isupper - check if a character is uppercase
- * @c: the character to check
+ * _isupper - Checks if a character is uppercase.
+ * @c: The character to be checked.
  *
- * Return: 1 if c is uppercase, 0 otherwise
+ * Return: 1 if the character is uppercase, 0 otherwise.
  */
 int _isupper(int c)
 {
-	/* ASCII codes for uppercase letters are 65 to 90 */
-	if (c >= 65 && c <= 90)
-		return (1);
-	else
-		return (0);
+       	return (c >= 'A' && c <= 'Z') ? 1 : 0;
 }
-
+ 
