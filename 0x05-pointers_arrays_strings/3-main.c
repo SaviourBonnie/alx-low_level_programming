22335b119b50b318fb6e3c93cfa5d4ece9d3aa72
SaviourBonnie <saviourbonnie@gmail.com>
2023-07-24 03:55:45 -0700

    str pointers

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Test the _puts function.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
