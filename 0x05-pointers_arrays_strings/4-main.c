ac12ff9fa0fb518ca300107fa894d67e29913e8a
SaviourBonnie <saviourbonnie@gmail.com>
2023-07-24 04:28:37 -0700

    i'm tired

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Test the print_rev function.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	print_rev(str);
+	return (0);
+}
