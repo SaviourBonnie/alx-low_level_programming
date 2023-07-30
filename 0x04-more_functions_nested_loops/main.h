b80deda0427f72ed3fa24c307264957ea4bcde70
SaviourBonnie <saviourbonnie@gmail.com>
2023-07-30 16:04:44 -0700

    square git commit -m square git commit -m square

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,4 +9,6 @@ void print_numbers(void);
 void print_most_numbers(void);
 int _putchar(char c);
 void print_diagonal(int n);
+int _putchar(char c);
+void print_square(int size);
 #endif /* MAIN_H */
