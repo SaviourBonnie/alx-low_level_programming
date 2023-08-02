4e8b8b9b76e51539bda6e2a69a808abe5f9293b7
SaviourBonnie <saviourbonnie@gmail.com>
2023-08-02 08:04:01 -0700

    REcussion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0') /* Base case: If we reach the end of the string (null terminator), print a new line and return. */
+	if (*s == '\0') /* Base case: If we reach the end of the string */
 	{
 		_putchar('\n');
 		return;
 	}
 
 	_putchar(*s); /* Print the current character of the string. */
-	_puts_recursion(s + 1); /* Recursively call the function with the next character (move to the next position in the string). */
+	_puts_recursion(s + 1); /* Recursively calling */
 }
