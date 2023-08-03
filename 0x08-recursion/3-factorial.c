4e5dcaf34ab0110e56ee4d40e6ddba5d02eac2fd
SaviourBonnie <saviourbonnie@gmail.com>
2023-08-02 18:37:56 -0700

    do

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,19 +4,20 @@
  * factorial - Returns the factorial of a given number.
  * @n: The number for which the factorial is to be calculated.
  *
- * Return: If n is lower than 0, return -1 (error).*/
+ * Return: The factorial of the number. If n is lower than 0, return -1 (error).
+ */
 int factorial(int n)
 {
-	if (n < 0) /* Base case: If n is lower than 0, return -1 (error).*/
+	if (n < 0) /* Base case: If n is lower than 0, return -1 (error). */
 	{
 		return (-1);
 	}
-	else if (n == 0) /* Base case: Factorial of 0 is 1.*/
+	else if (n == 0) /* Base case: Factorial of 0 is 1. */
 	{
 		return (1);
 	}
-	else /* Recursive case: Calculate factorial using recursion.*/
+	else /* Recursive case: Calculate factorial using recursion. */
 	{
-		return n * factorial(n - 1);
+		return (n * factorial(n - 1));
 	}
 }
