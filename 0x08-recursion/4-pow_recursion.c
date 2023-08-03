0923f50e403ad296e8b16aab32bb8ff98070db3d
SaviourBonnie <saviourbonnie@gmail.com>
2023-08-02 18:50:43 -0700

    tt

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,14 +11,14 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0) /* Base case: If y is lower than 0, return -1 (error). */
 	{
-		return -1;
+		return (-1);
 	}
 	else if (y == 0) /* Base case: Any number raised to 0 is 1. */
 	{
-		return 1;
+		return (1);
 	}
 	else /* Recursive case: Calculate power using recursion. */
 	{
-		return x * _pow_recursion(x, y - 1);
+		return (x * _pow_recursion(x, y - 1));
 	}
 }
