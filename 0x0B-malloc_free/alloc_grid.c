95e727809d0f79967867c8c182937e599e0d0adc
SaviourBonnie <saviourbonnie@gmail.com>
2023-08-10 00:57:31 +0000

    htrh

---
+++ 0x0B-malloc_free/alloc_grid.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int **alloc_grid(int width, int height)
+{
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	int **grid = (int **)malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
+
+	for (int i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			// If memory allocation fails for a row, free previously allocated rows
+			for (int j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
+		}
+
+		for (int j = 0; j < width; j++)
+		{
+			grid[i][j] = 0; // Initialize each element to 0
+		}
+	}
+
+	return (grid);
+}
+
+int main()
+{
+	int width = 4;
+	int height = 3;
+
+	int **grid = alloc_grid(width, height);
+
+	if (grid == NULL)
+	{
+		printf("Memory allocation failed.\n");
+		return (1);
+	}
+
+	// Printing the allocated grid
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			printf("%d ", grid[i][j]);
+		}
+		printf("\n");
+	}
+
+	// Freeing the allocated memory
+	for (int i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+
+	return (0);
+}
