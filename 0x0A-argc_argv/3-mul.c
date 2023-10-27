6cccf8e88aba22901478f8dd842a5cd10190b2d5
SaviourBonnie <saviourbonnie@gmail.com>
2023-10-27 23:38:10 +0000

    main mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of arguments, including the program name
+ * @argv: An array of strings containing the arguments
+ *
+ * Return: 0 for success, 1 for error
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2);
+
+	int result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
