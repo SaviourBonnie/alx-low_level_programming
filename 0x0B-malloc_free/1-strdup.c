20f3951d67296467ad6292774bf65892ac9055db
SaviourBonnie <saviourbonnie@gmail.com>
2023-11-02 01:53:27 +0000

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,24 +7,33 @@
  * Return: Pointer to a newly allocated space containing a duplicate of the
  * string, or NULL if memory allocation fails or if str is NULL
  */
-char *_strdup(char *str) {
+char *_strdup(char *str)
+{
 	char *duplicate;
 	unsigned int length = 0;
 	unsigned int i;
 
 	if (str == NULL)
+	{
 		return (NULL);
+	}
 
 	while (str[length] != '\0')
+	{
 		length++;
+	}
 
 	duplicate = (char *)malloc((length + 1) * sizeof(char));
 
 	if (duplicate == NULL)
+	{
 		return (NULL);
+	}
 
 	for (i = 0; i <= length; i++)
+	{
 		duplicate[i] = str[i];
+	}
 
 	return (duplicate);
 }
